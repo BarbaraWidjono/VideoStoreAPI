5c627d1ef63978e8b5a482bb4301505f3daab107
BarbaraWidjono <barbarawidjono@gmail.com>
2018-11-05 12:14:48 -0800

    Changed show page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,14 +1,14 @@
 class MoviesController < ApplicationController
   def index
     movies = Movie.all
-    render json: movify(movies), status: :ok
+    render json: movify_index(movies), status: :ok
   end
 
   def show
     movie_id = params[:id]
     movie = Movie.find_by(id: movie_id)
     if movie
-      render json: movify(movie), status: :ok
+      render json: movify_show(movie), status: :ok
     else
       render_error(:not_found, {movie_id: ["No such movie"]})
     end
@@ -16,7 +16,11 @@ class MoviesController < ApplicationController
 
   private
 
-  def movify(movie_data)
+  def movify_index(movie_data)
     return movie_data.as_json(only: [:id, :title, :release_date])
+  end
+
+  def movify_show(movie_data)
+    return movie_data.as_json(only: [:title, :overview, :release_date, :inventory])
   end
 end
