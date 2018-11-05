334314c000d0f8ed7fdf45d22d641137e78c5238
Pushpa Agr <pushpaagr108@gmail.com>
2018-11-05 14:44:51 -0800

    added model realtions and added rentals table with foreign keys

---
+++ db/migrate/20181105224238_add_foreign_key_to_rentals.rb
@@ -0,0 +1,8 @@
+class AddForeignKeyToRentals < ActiveRecord::Migration[5.2]
+  def change
+
+    add_reference :rentals, :movie, foreign_key: true
+    add_reference :rentals, :customer, foreign_key: true
+
+  end
+end
