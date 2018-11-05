648263c10e1a9d8cb8f5e8090c7892ea0ecb687a
BarbaraWidjono <barbarawidjono@gmail.com>
2018-11-05 12:10:09 -0800

    Added Movie#index, Movie#show

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,12 @@
 class CustomersController < ApplicationController
+  def index
+    customers = Customer.all
+    render json: customerify(customers), status: :ok
+  end
+
+  private
+
+  def customerify(customer_data)
+    return customer_data.as_json(only: [:id, :name, :registered_at, :postal_code, :phone])
+  end
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,22 @@
 class MoviesController < ApplicationController
+  def index
+    movies = Movie.all
+    render json: movify(movies), status: :ok
+  end
+
+  def show
+    movie_id = params[:id]
+    movie = Movie.find_by(id: movie_id)
+    if movie
+      render json: movify(movie), status: :ok
+    else
+      render_error(:not_found, {movie_id: ["No such movie"]})
+    end
+  end
+
+  private
+
+  def movify(movie_data)
+    return movie_data.as_json(only: [:id, :title, :release_date])
+  end
 end
