b16d71969dc250d0b50ff1062b3fbb352d20f4b6
Pushpa Agr <pushpaagr108@gmail.com>
2018-11-05 12:20:09 -0800

    added customer#show method

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -4,9 +4,28 @@ class CustomersController < ApplicationController
     render json: customerify(customers), status: :ok
   end
 
+
+
+  def show
+    customer_id = params[:id]
+    customer = Customer.find_by(id: customer_id)
+    if customer
+      render json: customerify(customer), status: :ok
+    else
+      render_error(:not_found, {customer_id: ["No such Customer"]})
+    end
+  end
+
+
   private
 
   def customerify(customer_data)
     return customer_data.as_json(only: [:id, :name, :registered_at, :postal_code, :phone])
   end
+
+
+  # def customerify_show(customer_data)
+  #   return customer_data.as_json(only: [:title, :overview, :release_date, :inventory])
+  # end
+
 end
