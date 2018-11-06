b3d679413b86054b2f5c1432d4004c74b8d06936
BarbaraWidjono <barbarawidjono@gmail.com>
2018-11-05 16:12:04 -0800

    Customer controller tests

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,7 +1,30 @@
 require "test_helper"
 
 describe CustomersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe 'index' do
+    it 'should get index' do
+      get customers_path
+      must_respond_with :success
+    end
+  end
+
+  describe 'show' do
+
+    before do
+      @customer = Customer.first
+    end
+
+    it 'will show an existing customer' do
+
+      get customer_path(@customer.id)
+      must_respond_with :success
+    end
+
+    it 'will render a status not_found for a non-existant customer' do
+      destroyed_customer = @customer.destroy
+      destroyed_customer.save
+      get customer_path(destroyed_customer.id)
+      must_respond_with :not_found
+    end
+  end
 end
