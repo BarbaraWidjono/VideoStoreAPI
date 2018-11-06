82d7e0971cc4f29142991a2d436b5eed1356ae82
Pushpa Agr <pushpaagr108@gmail.com>
2018-11-06 14:24:47 -0800

    added duedate and checkout date to rentals table

---
+++ db/migrate/20181106222107_add_column_checkout_duedate_rentals.rb
@@ -0,0 +1,7 @@
+class AddColumnCheckoutDuedateRentals < ActiveRecord::Migration[5.2]
+  def change
+    add_column :rentals, :check_out_date, :date
+    add_column :rentals, :due_date, :date
+
+  end
+end
