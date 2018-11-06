9acf14a1946355869d31371b5ef19f52bf7900af
Pushpa Agr <pushpaagr108@gmail.com>
2018-11-06 14:20:49 -0800

    moved method from movie model back to rental controller

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -3,8 +3,7 @@ class RentalsController < ApplicationController
     rental = Rental.new(rental_params)
     if rental.save
       change_avail_inventory(rental_params[:movie_id])
-      # binding.pry
-      render json: { id: rental.id }
+      render json: { id: rental}
     else
       render_error(:bad_request, {rental: ["no such rental"]})
     end
@@ -15,5 +14,10 @@ class RentalsController < ApplicationController
     params.require(:rental).permit(:movie_id, :customer_id)
   end
 
+  def change_avail_inventory(id)
+    movie = Movie.find_by(id: id)
+    movie.available_inventory -= 1
+    movie.save
+  end
 
 end
