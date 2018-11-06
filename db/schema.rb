fc063b666f9287b9a8940d7cbc4ac12cf8114af3
Pushpa Agr <pushpaagr108@gmail.com>
2018-11-05 16:50:00 -0800

    added movies avail inventory column, and customer checkout count

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_05_224238) do
+ActiveRecord::Schema.define(version: 2018_11_06_004626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2018_11_05_224238) do
     t.string "state"
     t.string "postal_code"
     t.string "phone"
+    t.integer "movies_checked_out_count", default: 0
   end
 
   create_table "movies", force: :cascade do |t|
@@ -34,6 +35,7 @@ ActiveRecord::Schema.define(version: 2018_11_05_224238) do
     t.string "overview"
     t.date "release_date"
     t.integer "inventory"
+    t.integer "available_inventory"
   end
 
   create_table "rentals", force: :cascade do |t|
