0dacb7994257227006e7024a2b84a52b0192dff9
BarbaraWidjono <barbarawidjono@gmail.com>
2018-11-06 14:29:35 -0800

    Moved change available method

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -15,5 +15,9 @@ class RentalsController < ApplicationController
     params.require(:rental).permit(:movie_id, :customer_id)
   end
 
-
+  def change_avail_inventory(id)
+    movie = Movie.find_by(id: id)
+    movie.available_inventory -= 1
+    movie.save
+  end
 end
