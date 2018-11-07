d3d006ff837b95f0f7a48932816913c7a602cf3c
Pushpa Agr <pushpaagr108@gmail.com>
2018-11-06 16:48:02 -0800

    added rental model testing, all tests pass

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,9 +1,22 @@
 require "test_helper"
 
 describe Rental do
-  let(:shelly) {rentals(:one)}
+  let(:rental_one) {rentals(:one)}
 
-  it "must be valid" do
-    value(shelly).must_be :valid?
+  it "Rental must be valid" do
+    value(rental_one).must_be :valid?
   end
+
+
+  it "rental belongs to customer" do
+    expect(rental_one.customer).must_be_kind_of Customer
+    expect(rental_one.customer.name).must_equal "Shelly Rocha"
+  end
+
+  it "rental belongs to movie" do
+    expect(rental_one.movie).must_be_kind_of Movie
+    expect(rental_one.movie.title).must_equal "Blacksmith of the Orange"
+  end
+
+
 end
