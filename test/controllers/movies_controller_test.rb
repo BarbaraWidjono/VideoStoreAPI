39323937a049159f08b60f635adb9631ce06afcd
Pushpa Agr <pushpaagr108@gmail.com>
2018-11-06 17:05:20 -0800

    added movies_controller tests

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -4,4 +4,43 @@ describe MoviesController do
   # it "must be a real test" do
   #   flunk "Need real tests"
   # end
+
+  describe "index" do
+    it "is a real working route" do
+      get movies_path
+      must_respond_with :success
+    end
+
+    it "return json" do
+      get movies_path
+      expect(response.header['Content-Type']).must_include 'json'
+    end
+
+    it "return an Array" do
+      get movies_path
+
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+    end
+
+    it "returns all of the movies" do
+      get movies_path
+
+      body = JSON.parse(response.body)
+      body.length.must_equal Movie.count
+    end
+
+
+    it "returns pets with exactly the required fields" do
+      keys = ["id", "title", "release_date"].sort
+      get movies_path
+      body = JSON.parse(response.body)
+      body.each do |movie|
+        movie.keys.sort.must_equal keys
+      end
+    end
+
+
+  end
+
 end
