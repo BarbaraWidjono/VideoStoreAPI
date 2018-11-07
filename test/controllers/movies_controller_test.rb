023c71d59e3087f2f06c7b742a273fd05a7d0cf0
Pushpa Agr <pushpaagr108@gmail.com>
2018-11-07 09:32:41 -0800

    all tests for movie passing

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -39,8 +39,81 @@ describe MoviesController do
         movie.keys.sort.must_equal keys
       end
     end
-
-
   end
+
+
+  describe "create" do
+    let(:movie_data) {
+      {
+        title: "new movie",
+        overview: "best movie ever",
+        release_date: "2018-11-02",
+        inventory: 10
+      }
+    }
+
+    it "create a new a movie given valid data" do
+      expect{
+        post movies_path, params: { movie: movie_data }
+      }.must_change "Movie.count", 1
+
+      body = JSON.parse(response.body)
+      expect(body).must_be_kind_of Hash
+      expect(body).must_include "id"
+
+      movie = Movie.find(body["id"].to_i)
+      expect(movie.title).must_equal movie_data[:title]
+      must_respond_with :success
+    end
+
+
+
+
+    it "returns an error for invalid movie data" do
+      # arrange
+      movie_data["title"] = nil
+
+      expect {
+        post movies_path, params: { movie: movie_data }
+      }.wont_change "Movie.count"
+
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_kind_of Hash
+      expect(body).must_include "errors"
+      expect(body["errors"]).must_include "title"
+      must_respond_with :bad_request
+    end
+  end
+
+  describe "show" do
+    let(:a_movie) {movies(:one)}
+    KEYS = ["title", "overview", "release_date", "inventory", "available_inventory"].sort
+
+    it "can get a movie" do
+      get movie_path(a_movie.id)
+      must_respond_with :success
+
+      body = JSON.parse(response.body)
+      expect(body).must_be_kind_of Hash
+      expect(body.keys.sort).must_equal KEYS
+    end
+
+    it "rendors error not_found, when a movie does not exist" do
+
+      no_movie_id = 33333
+      get movie_path(no_movie_id)
+
+      body = JSON.parse(response.body)
+      expect(body).must_be_kind_of Hash
+      expect(body).must_include "errors"
+      expect(body["errors"]).must_include "movie_id"
+      must_respond_with :not_found
+    end
+  end
+
+  
+
+
 
 end
