24f7eeb953f831b923d834b15cc8b908f18c860c
BarbaraWidjono <barbarawidjono@gmail.com>
2018-11-07 11:33:57 -0800

    Rentals#create tests

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -7,15 +7,27 @@ describe RentalsController do
     let(:movie_one) {movies(:one)}
 
     it 'will create a rental with valid customer and movie data' do
-      rental = {
-        rental: {
-          customer: cust_one.id,
-          movie: movie_one.id
+       info =
+       {
+          "customer_id" => cust_one.id,
+          "movie_id" => movie_one.id
         }
-      }
 
-      expect{post rentals_path, params: rental}.must_change('Rental.count', +1)
+      expect{post checkout_path, params: info}.must_change('Rental.count', +1)
+
     end
 
+    it 'a valid rental will increment the customers movies_checked_out_count' do
+       info =
+       {
+          "customer_id" => cust_one.id,
+          "movie_id" => movie_one.id
+        }
+        post checkout_path, params: info
+        # This reloads the yaml file with the new changes to the variable specified. In this example, it reloaded cust_one data with the changes after checkout_path occurred
+        cust_one.reload
+
+        expect(cust_one.movies_checked_out_count).must_equal 1
+    end
   end
 end
